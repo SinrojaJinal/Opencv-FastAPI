11fe0a855b48f5eb32aceec2e2d9143ab3117c4d
Jinal Sinroja - Bacancy <79623020+SinrojaJinal@users.noreply.github.com>
2021-03-17 11:13:03 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.6
+
+WORKDIR /usr/src/app
+
+COPY . .
+
+RUN pip3 install --no-cache-dir -r requirements.txt
+
+CMD ["python","./main.py"]
---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, UploadFile, File
+import cv2 as cv
+import uvicorn
+import os
+from bgrtogray import GrayScale
+
+app = FastAPI()
+
+@app.post("/uploadfile")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        os.mkdir("files")
+        print(os.getcwd()) # os.getcwd() method tells us the location of current working directory (CWD).
+    except Exception as e:
+        print(e) 
+    file_name = os.getcwd()+"/files/"+file.filename.replace(" ", "-")
+    with open(file_name,'wb+') as f:
+        f.write(file.file.read())
+        f.close()
+    obj = GrayScale()
+    obj.GrayScaling(file_name)  
+    return {"filename": file_name}
+
+if __name__ =="__main__":
+    uvicorn.run(app, port=8001)
