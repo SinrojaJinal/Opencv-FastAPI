03ca95f0add0869fe780eff211511add4a3c157c
Jinal Sinroja - Bacancy <79623020+SinrojaJinal@users.noreply.github.com>
2021-03-17 17:23:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ async def upload_file(file: UploadFile = File(...)):
     return {"filename": file_name}
 
 if __name__ =="__main__":
-    uvicorn.run(app, port=8001)
+    uvicorn.run(app, host = '0.0.0.0', port=8001)
